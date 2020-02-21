50f0fd8169e8b3f355a06e8c36656affdec291c9
fgisslen <fgisslen@kth.se>
2020-02-21 18:28:42 +0100

    mine blocks

--- main.js
+++ main.js
@@ -7,15 +7,25 @@ class Block{
     this.data = data;
     this.previousHash = previousHash;
     this.hash = this.calculateHash();
+    this.nonce = 0;
   }
   calculateHash(){
-    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)+this.nonce).toString();
+  }
+
+  mineBlock(difficulty){
+    while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
+      this.nonce++;
+      this.hash = this.calculateHash();
+    }
+    console.log("Block mined: " + this.hash);
   }
 }
 
 class Blockchain{
   constructor(){
     this.chain = [this.createGenesisBlock()];
+    this.difficulty = 4;
   }
   createGenesisBlock(){
     return new Block(0, "01/01/2020", "Genesis block", "0")
@@ -25,7 +35,7 @@ class Blockchain{
   }
   addBlock(newBlock){
     newBlock.previousHash = this.getLatestBlock().hash;
-    newBlock.hash = newBlock.calculateHash();
+    newBlock.mineBlock(this.difficulty);
     this.chain.push(newBlock);
   }
   isChainValid(){
@@ -44,13 +54,7 @@ class Blockchain{
 }
 
 let bc = new Blockchain();
+console.log("Mining block 1...");
 bc.addBlock(new Block(1, "15/02/2020", {id: 5}));
+console.log("Mining block 2...");
 bc.addBlock(new Block(2, "17/02/2020", {id: 9}));
-
-console.log('Is blockchain valid? ' + bc.isChainValid());
-
-bc.chain[1].data = {id: 30};
-bc.chain[1].hash = bc.chain[1].calculateHash();
-console.log('Is blockchain valid? ' + bc.isChainValid());
-
-//console.log(JSON.stringify(bc, null, 4))
